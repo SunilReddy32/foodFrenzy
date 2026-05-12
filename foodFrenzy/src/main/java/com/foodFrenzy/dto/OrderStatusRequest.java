826beea8a65f6e5b76fc2a0d170de02fdb4d43c6
Sunil Reddy Chinnabianagari <120704733+SunilReddy32@users.noreply.github.com>
2026-05-12 12:47:19 +0530

    Delete foodFrenzy directory

--- foodFrenzy/src/main/java/com/foodFrenzy/dto/OrderStatusRequest.java
+++
@@ -1,9 +0,0 @@
-package com.foodFrenzy.dto;
-
-public class OrderStatusRequest {
-    private String status;
-    
-    public String getStatus() {
-        return status;
-    }
-}
--- foodFrenzy/src/main/java/com/foodFrenzy/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.foodFrenzy.repository;
-import com.foodFrenzy.entity.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-  
-}
